3527240ee5a23d71b7139a795d05c5a26c501761
cmad-06 <vibhor.cmad6@gmail.com>
2018-02-24 12:07:52 +0530

    test commit

--- src/main/java/com/cisco/automation/modules/session_mon/TestSessionMon.java
+++ src/main/java/com/cisco/automation/modules/session_mon/TestSessionMon.java
@@ -1,11 +1,25 @@
 package com.cisco.automation.modules.session_mon;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestSessionMon {
 
-	@Test
-	public void Test1(){
-		
+	@Test(description="my first session mon test case", dependsOnMethods={"TestGrouping1"})
+	public void TestDependency1(){
+		System.out.println("Session mon test1");
+		int res = 11;
+		Assert.assertEquals(res, 10);  //fails here
+		System.out.println("Inv test1 2nd assert");	//never executed
+
+	}
+	
+	@Test(description="my second session mon test case")
+	public void TestGrouping1(){
+		System.out.println("Session mon test2");
+		int res = 11;
+		Assert.assertEquals(res, 10);  //fails here
+		System.out.println("Inv test1 2nd assert");	//never executed
+
 	}
 }
