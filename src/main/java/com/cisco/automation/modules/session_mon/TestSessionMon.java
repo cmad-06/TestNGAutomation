54bd08b4a3af3345a8c5a60a25fcf05ba3770ba1
cmad-06 <vibhor.cmad6@gmail.com>
2018-02-24 12:12:45 +0530

    adding depdendent test cases

--- src/main/java/com/cisco/automation/modules/session_mon/TestSessionMon.java
+++ src/main/java/com/cisco/automation/modules/session_mon/TestSessionMon.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class TestSessionMon {
 
-	@Test(description="my first session mon test case", dependsOnMethods={"TestGrouping1"})
+	@Test(description="my first session mon test case", dependsOnMethods={"TestDependency2"})
 	public void TestDependency1(){
 		System.out.println("Session mon test1");
 		int res = 11;
@@ -15,11 +15,21 @@ public class TestSessionMon {
 	}
 	
 	@Test(description="my second session mon test case")
-	public void TestGrouping1(){
+	public void TestDependency2(){
 		System.out.println("Session mon test2");
 		int res = 11;
 		Assert.assertEquals(res, 10);  //fails here
 		System.out.println("Inv test1 2nd assert");	//never executed
 
 	}
+	
+	@Test(description="my third session mon test case")
+	public void TestDependency3(){
+		System.out.println("Session mon test3");
+		int res = 11;
+		Assert.assertEquals(res, 10);  //fails here
+		System.out.println("Inv test1 2nd assert");	//never executed
+
+	}
+	
 }
