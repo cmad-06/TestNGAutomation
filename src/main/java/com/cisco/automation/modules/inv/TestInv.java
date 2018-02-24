eedc593c1b1574c2f64291e2f5caef55a021bf47
cmad-06 <vibhor.cmad6@gmail.com>
2018-02-24 13:32:50 +0530

    param demoed

--- src/main/java/com/cisco/automation/modules/inv/TestInv.java
+++ src/main/java/com/cisco/automation/modules/inv/TestInv.java
@@ -1,16 +1,31 @@
 package com.cisco.automation.modules.inv;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class TestInv {
 
+	
+	private String baseURL;
+	
 //---------------------------------------------------------------------------------------------
 	
+	@BeforeClass
+	@Parameters({"server_ip"})
+	public void preClassSetup(String ip){
+		System.out.println("inside before class");
+		baseURL = "https://"+ip+":8443/emsam";
+	}
+	
+//---------------------------------------------------------------------------------------------	
+	
+	
 	@BeforeMethod
-	public void preSetup(){
+	public void preMethodSetup(){
 		System.out.println("inside before method");
 	}
 	
@@ -18,9 +33,12 @@ public class TestInv {
 	
 	@Test(description="my first test case")
 	public void TestSoftAssertion(){
+		
+		System.out.println("Base URL is: "+baseURL);
+		
 		SoftAssert sa = new SoftAssert();
 		System.out.println("Inv test1");
-		sa.assertEquals(10, 11);		//soft assertion, continue even on failure
+		sa.assertEquals(10, 10);		//soft assertion, continue even on failure
 		System.out.println("Inv test1 2nd assert");
 		sa.assertEquals(10, 10);
 		sa.assertAll();
@@ -31,8 +49,8 @@ public class TestInv {
 	@Test(description="my second test case")
 	public void TestHardAssertion(){
 		System.out.println("Inv test1");
-		int res = 11;
-		Assert.assertEquals(res, 10);  //fails here
+		int res = 10;
+		Assert.assertEquals(res, 10);  //if fails here, exit
 		System.out.println("Inv test1 2nd assert");	//never executed
 
 	}
