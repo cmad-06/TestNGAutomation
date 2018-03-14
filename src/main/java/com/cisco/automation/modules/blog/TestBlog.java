fffc873f99c3cdb1c0bf0f8fa45e1b9099f8ee83
vjcalling <vibhor.jain@outlook.com>
2018-03-14 08:13:51 +0530

    adding test cases

--- src/main/java/com/cisco/automation/modules/blog/TestBlog.java
+++ src/main/java/com/cisco/automation/modules/blog/TestBlog.java
@@ -11,6 +11,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class TestBlog extends CMADTest{
 	
+//----------------------------------------------------------------------------------------
+	
 	@Test(description="test blog ID getting populated automatically in DB")
 	public void TestBlogId(){
 		
@@ -21,6 +23,7 @@ public class TestBlog extends CMADTest{
 		response.then().assertThat().body("blogId", equalTo(1));
 	}
 	
+//----------------------------------------------------------------------------------------
 	
 	@Test(description="test blog title getting populated properly in DB")
 	public void TestBlogTitle(){
@@ -32,5 +35,30 @@ public class TestBlog extends CMADTest{
 		response.then().assertThat().body("blogTitle", equalTo("Sample title2"));
 	}
 	
+//----------------------------------------------------------------------------------------
 	
+	@Test(description="test blog author getting populated properly in DB")
+	public void TestBlogAuthor(){
+		
+		RequestSpecification httpRequest = CMADTest.getBasicRequestInstance();
+		
+		Response response = httpRequest.request(Method.GET, "/service/blog/2");
+		response.prettyPrint();
+		response.then().assertThat().body("blogAuthor", equalTo("User"));
+	}
+
+//----------------------------------------------------------------------------------------
+	
+	@Test(description="test blog likes getting populated properly in DB")
+	public void TestBlogLikes(){
+		
+		RequestSpecification httpRequest = CMADTest.getBasicRequestInstance();
+		
+		Response response = httpRequest.request(Method.GET, "/service/blog/2");
+		response.prettyPrint();
+		response.then().assertThat().body("blogLikes", equalTo(0));
+	}
+	
+//----------------------------------------------------------------------------------------	
+
 }
--- src/main/java/com/cisco/automation/modules/user/TestUser.java
+++ src/main/java/com/cisco/automation/modules/user/TestUser.java
@@ -1,11 +1,66 @@
 package com.cisco.automation.modules.user;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.cisco.automation.modules.global.CMADTest;
 
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 public class TestUser extends CMADTest{
 
+//----------------------------------------------------------------------------------------	
+
+	@Test(description="test user first name getting populated properly in DB")
+	public void TestFirstName(){
+		
+		RequestSpecification httpRequest = CMADTest.getBasicRequestInstance();
+		
+		Response response = httpRequest.request(Method.GET, "/service/user/1");
+		response.prettyPrint();
+		response.then().assertThat().body("firstName", equalTo("Vibhor"));
+	}
+
+//----------------------------------------------------------------------------------------
+	
+	@Test(description="test user last name getting populated properly in DB")
+	public void TestLastName(){
+		
+		RequestSpecification httpRequest = CMADTest.getBasicRequestInstance();
+		
+		Response response = httpRequest.request(Method.GET, "/service/user/1");
+		response.prettyPrint();
+		response.then().assertThat().body("lastName", equalTo("Jain"));
+	}
+
+//----------------------------------------------------------------------------------------
+	
+	@Test(description="test username getting populated properly in DB")
+	public void TestUserName(){
+		
+		RequestSpecification httpRequest = CMADTest.getBasicRequestInstance();
+		
+		Response response = httpRequest.request(Method.GET, "/service/user/1");
+		response.prettyPrint();
+		response.then().assertThat().body("username", equalTo("vjcalling"));
+	}
+
+//----------------------------------------------------------------------------------------
+
+	@Test(description="test user email getting populated properly in DB")
+	public void TestUserEmail(){
+		
+		RequestSpecification httpRequest = CMADTest.getBasicRequestInstance();
+		
+		Response response = httpRequest.request(Method.GET, "/service/user/1");
+		response.prettyPrint();
+		response.then().assertThat().body("email", equalTo("vibhjain@cisco.com"));
+	}
+	
+//----------------------------------------------------------------------------------------	
 	
 }
